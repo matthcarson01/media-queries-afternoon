47236973d616b7d18c602e9ebbba11544deb4203
mcarson01 <matthcarson01@gmail.com>
2017-11-06 20:55:09 -0600

    removed comments

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,6 @@
 import React,{Component} from 'react';
 import './Navbar.css';
 
-// export default function Navbar (){
-//     return(
-//         <nav>
-//             <div className="HamburgerMenu btn">
-//                 <span>Menu</span>
-//                 <i className="fa fa-bars fa-1x" aria-hidden="true"></i>
-//             </div>
-            
-//             <ul>
-//                 <li><a>SERVICES</a></li>
-//                 <li><a>PORTFOLIO</a></li>
-//                 <li><a>ABOUT</a></li>
-//                 <li><a>TEAM</a></li>
-//                 <li><a>CONNECT</a></li>
-//             </ul>         
-//         </nav>
-//     )
-// }
 export default class Navbar extends Component {
     constructor(props){
         super(props);
